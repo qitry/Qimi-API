45229444df3232d43fc137ddf4a8a4000d361c83
MornZe <mengze2@foxmail.com>
2026-04-15 14:19:08 +0800

    fix: 删除 api/openapi.ts 避免构建冲突

--- api/openapi.ts
+++
@@ -1,7 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import openapi from './openapi-spec.json';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.setHeader('Content-Type', 'application/json');
-  return res.status(200).json(openapi);
-}
